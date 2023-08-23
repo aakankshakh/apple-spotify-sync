5803b5c23c5dc2e44c179478c820819c1badfee5
Calum Bird <calum@trelent.net>
2023-08-23 20:48:10 -0300

    Add api endpoints

---
+++ src/lib/server/crud.ts
@@ -0,0 +1,118 @@
+import { kv } from "@vercel/kv";
+import { playlist, sync } from "@/types";
+
+export async function createPlaylist(playlist: playlist): Promise<void> {
+  try {
+    await kv.hset(`playlist:${playlist.id}`, playlist);
+  } catch (error) {
+    // Handle errors
+  }
+}
+
+/**
+ * Retrieve an existing playlist from the database
+ */
+export async function getPlaylist(id: string): Promise<playlist | null> {
+  try {
+    const playlistData: playlist | null | undefined = await kv.hget(
+      `playlist:${id}`,
+      "playlist"
+    );
+    return playlistData ? JSON.parse(JSON.stringify(playlistData)) : null;
+  } catch (error) {
+    // Handle errors
+    return null;
+  }
+}
+
+/**
+ * Soft delete a playlist by setting the deleted_at field to the current date.
+ */
+export async function deletePlaylist(id: string): Promise<void> {
+  try {
+    const today = new Date();
+    const playlist = await getPlaylist(id);
+    if (playlist) {
+      const newPlaylist = { ...playlist, deleted_at: today };
+      await kv.hset(`playlist:${id}`, newPlaylist);
+    }
+  } catch (error) {
+    // Handle errors
+  }
+}
+
+/**
+ * Update a playlist by merging the modified properties with the current playlist.
+ */
+export async function updatePlaylist(
+  id: string,
+  updatedPlaylist: Partial<playlist>
+): Promise<void> {
+  try {
+    const currentPlaylist = await getPlaylist(id);
+    if (currentPlaylist) {
+      const newPlaylist = { ...currentPlaylist, ...updatedPlaylist };
+      await kv.hset(`playlist:${id}`, newPlaylist);
+    }
+  } catch (error) {
+    // Handle errors
+  }
+}
+
+/**
+ * Create a new sync in the database
+ */
+export async function createSync(sync: sync): Promise<void> {
+  try {
+    await kv.hset(`sync:${sync.playlist_id}`, sync);
+  } catch (error) {
+    // Handle errors
+  }
+}
+
+/**
+ * Soft delete a sync by setting the deleted_at field to the current date.
+ */
+export async function deleteSync(playlist_id: string): Promise<void> {
+  try {
+    const today = new Date();
+    const sync = await getSync(playlist_id);
+    if (sync) {
+      const newSync = { ...sync, deleted_at: today };
+      await kv.hset(`sync:${playlist_id}`, newSync);
+    }
+  } catch (error) {
+    // Handle errors
+  }
+}
+
+/**
+ * Update a sync by merging the modified properties with the current sync.
+ */
+export async function updateSync(
+  playlist_id: string,
+  updatedSync: Partial<sync>
+): Promise<void> {
+  try {
+    const currentSync = await getSync(playlist_id);
+    if (currentSync) {
+      const newSync = { ...currentSync, ...updatedSync };
+      await kv.hset(`sync:${playlist_id}`, newSync);
+    }
+  } catch (error) {
+    // Handle errors
+  }
+}
+
+/**
+ * Retrieve an existing sync from the database
+ */
+export async function getSync(playlist_id: string): Promise<sync | null> {
+  try {
+    const syncData = await kv.hget(`sync:${playlist_id}`, "sync");
+    return syncData ? JSON.parse(JSON.stringify(syncData)) : null;
+  } catch (error) {
+    // Handle errors
+    return null;
+  }
+}
--- src/pages/api/playlist.ts
+++ src/pages/api/playlist.ts
@@ -1,12 +1,61 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { ResponseObj } from "@/types";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { v4 as uuidv4 } from "uuid";
+import { createPlaylist, getPlaylist } from "@/lib/server/crud";
+import { ResponseObject, playlist } from "@/types";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseObj>
+  res: NextApiResponse<ResponseObject>
 ) {
+  if (req.method === "GET") {
+    await get(req, res);
+  } else if (req.method === "POST") {
+    await post(req, res);
+  } else {
+    res.status(405).json({
+      object: "playlist",
+      data: undefined,
+      error: "Method not allowed",
+    });
+  }
+}
+
+/**
+ * Retrieve an existing playlist from the database
+ */
+const get = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseObject>
+) => {
+  const { id } = req.query;
+  const playlistData = await getPlaylist(id as string);
+  if (playlistData) {
+    res
+      .status(200)
+      .json({ object: "playlist", data: playlistData, error: undefined });
+  } else {
+    res.status(404).json({
+      object: "playlist",
+      data: undefined,
+      error: `Playlist:${id} not found`,
+    });
+  }
+};
+
+/**
+ * Create a new playlist in the database
+ */
+const post = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseObject>
+) => {
+  const playlistData: playlist = {
+    ...req.body,
+    id: uuidv4(), // Force a new id
+  };
+
+  await createPlaylist(playlistData);
   res
     .status(200)
-    .json({ object: "playlist", data: undefined, error: "not implemented" });
-}
+    .json({ object: "playlist", data: playlistData, error: undefined });
+};
