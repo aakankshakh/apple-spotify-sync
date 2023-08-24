22f23fb69ac005b0c7553a50263ecbd61847f7f3
aakanksha <akhandwaha@mta.ca>
2023-08-24 01:00:55 -0300

    Playlist page looks SOOO AWESOME NOW!! :-D

--- src/components/song.tsx
+++ src/components/song.tsx
@@ -9,13 +9,13 @@ interface SongProps {
 const Song = (props: SongProps) => {
   const { song, index } = props;
   const seconds = song.duration % 60;
+  const secondsString = seconds < 10 ? seconds + "0" : seconds;
   const minutes = (song.duration - seconds) / 60;
-  const duration: string = minutes + ":" + seconds;
+  const duration: string = minutes + ":" + secondsString;
 
   return (
     <li className="flex border-b border-gray-800 hover:bg-gray-800">
-      <div className="p-3 w-8 flex-shrink-0">▶️</div>
-      <div className="p-3 w-8 flex-shrink-0">❤️</div>
+      <div className="p-3 w-8 flex-shrink-0 text-gray-600">{index + 1}</div>
       <div className="p-3 w-full">{song.name}</div>
       <div className="p-3 w-full">{song.artist}</div>
       <div className="p-3 w-12 flex-shrink-0 text-right">{duration}</div>
