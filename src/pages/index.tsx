ccc5f6aa277f72f0b50c6fec9ba8d8b68f50232a
aakanksha <akhandwaha@mta.ca>
2023-08-26 15:45:33 -0300

    Sync !

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -132,7 +132,7 @@ export default function Home() {
         ) : (
           <div>
             <button className="text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:ring-orange-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center m-3">
-              SYNC A Playlist
+              Sync Playlist
             </button>
             <button
               type="button"
