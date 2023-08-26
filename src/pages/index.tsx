61595659b1129c32dcc05da14b90b04ab53a4e4b
aakanksha <akhandwaha@mta.ca>
2023-08-26 00:18:48 -0300

    playlist loading and unathenticated pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,7 +74,7 @@ export default function Home() {
         <div>
           <button
             type="button"
-            className="text-white bg-[#050708] dark:bg-[#777474] hover:bg-[#050708]/90 dark:hover:bg-[#777474]/60 focus:ring-4 focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 m-3"
+            className="text-white bg-[#050708] dark:bg-[#777474] hover:bg-[#050708]/90 dark:hover:bg-[#777474]/60 focus:ring-4 focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 m-3"
           >
             <svg
               className="mr-2 -ml-1 w-5 h-5"
@@ -95,7 +95,7 @@ export default function Home() {
           </button>
           <button
             type="button"
-            className="text-white bg-[#050708] dark:bg-[#777474] hover:bg-[#050708]/90 dark:hover:bg-[#777474]/60 focus:ring-4 focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 m-3"
+            className="text-white bg-[#050708] dark:bg-[#777474] hover:bg-[#050708]/90 dark:hover:bg-[#777474]/60 focus:ring-4 focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 m-3"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
